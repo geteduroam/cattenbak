33a3a5d9f6436a82c16bae1a174060e8168eab9b
Jørn Åne de Jong <git@jornane.no>
2020-06-30 20:11:49 +0200

    Move the catnip proxy to Frankfurt (closer to Amsterdam)

--- etc/cattenbak.conf.php
+++ etc/cattenbak.conf.php
@@ -12,7 +12,7 @@ function getOldGetEduroamProfile( $display, $name ) {
 }
 
 return [
-	'catnip' => 'https://mkrld6l9xi.execute-api.eu-north-1.amazonaws.com/catnip',
+	'catnip' => 'https://y1wtfr5sqa.execute-api.eu-central-1.amazonaws.com/catnip',
 	'versions' => [1/*, 2*/],
 	'countries' => ['AL', 'AM', 'AR', 'AT', 'AU', 'BE', 'BG', 'BR', 'CA', 'CH', 'CL', 'CO', 'CR', 'CZ', 'DE', 'DK', 'EC', 'EE', 'ES', 'FI', 'FR', 'GE', 'GEANT', 'GR', 'HR', 'HU', 'IE', 'IL', 'IS', 'IT', 'JP', 'KR', 'LK', 'LT', 'LU', 'LV', 'MA', 'ME', 'MK', 'MT', 'MW', 'MX', 'MY', 'NG', 'NL', 'NO', 'NZ', 'OM', 'PE', 'PH', 'PK', 'PL', 'PT', 'RS', 'RU', 'SE', 'SG', 'SI', 'SK', 'TR', 'UA', 'UG', 'UK', 'US', 'UY', 'ZA'],
 	'languages' => ['en', 'nb', 'es', 'de' /*, 'nl' */],
