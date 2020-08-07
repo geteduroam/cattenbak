35aaaa588ce30c5fd55d874aabce76e5ce78115d
Jørn Åne de Jong <git@jornane.no>
2020-08-07 20:22:20 +0200

    Bump cat library

--- src/eduroam/cat/device.php
+++ src/eduroam/cat/device.php
@@ -44,7 +44,7 @@ class Device
 		'android_nougat' => ['/Android 7[._][0-9]/'],
 		'android_oreo' => ['/Android 8[._][0-9]/'],
 		'android_pie' => ['/Android 9[._][0-9]/'],
-		'android_q' => ['/Android 10[._][0-9]/', '/Android 1[1-9]/', '/Android [2-9][0-9]/'],
+		'android_q' => ['/Android 10[._][0-9]/', '/Android 1[0-9]/', '/Android [2-9][0-9]/'],
 		0 => ['//'],
 	];
 
@@ -371,6 +371,7 @@ class Device
 	 */
 	public function getDownloadLink(): string
 	{
+		assert( !$this->isRedirect(), 'Impossible to get a download link for a redirect device' );
 		return $this->cat->getDownloadInstallerURL( $this->deviceID, $this->profileID );
 	}
 
