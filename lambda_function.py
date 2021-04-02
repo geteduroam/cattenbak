a3a69f05f3ee600837583d1562eda8a4e807c10c
Jørn Åne de Jong <jorn.dejong@uninett.no>
2021-04-03 00:22:05 +0200

    Use date-based serial, like in DNS

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,6 @@ from cattenbak import (
 	generate,
 	download_s3,
 	upload_s3,
-	get_old_discovery_from_url,
 	discovery_needs_refresh,
 )
 import boto3
@@ -12,19 +11,12 @@ import os
 def lambda_handler(event, context):
 	s3 = boto3.client("s3")
 	old_discovery = download_s3(s3, os.environ["s3_bucket"], os.environ["s3_path"])
-	if not "seq" in old_discovery or old_discovery["seq"] == 0:
-		print("WARNING: Unable to download old discovery from S3")
-		print("Fallback to downloading from discovery URL")
-		old_discovery = get_old_discovery_from_url()
+	old_serial = old_discovery["serial"] if "serial" in old_discovery else 0
 
-	# This may crash, but in that case we have lost our seq,
-	# so a crash is warranted
-	discovery = generate(seq=old_discovery["seq"] + 1)
+	discovery = generate(old_serial=old_serial)
 
-	if old_discovery:
-		discovery["seq"] = max(discovery["seq"], old_discovery["seq"] + 1)
 	if discovery_needs_refresh(old_discovery, discovery):
-		print("Uploading discovery seq %s" % discovery["seq"])
+		print("Uploading discovery serial %s" % discovery["serial"])
 		upload_s3(s3, discovery, os.environ["s3_bucket"], os.environ["s3_path"])
 	else:
-		print("Unchanged %d" % old_discovery["seq"])
+		print("Unchanged %d" % old_discovery["serial"])
