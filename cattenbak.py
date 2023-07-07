0e6a3e5276ca9c1b57cfad649c41639d853ba2b1
Jørn Åne de Jong <git@jornane.no>
2023-07-07 10:25:07 +0200

    First version of discovery v2

--- cattenbak.py
+++ cattenbak.py
@@ -4,218 +4,255 @@ import json
 import datetime
 import argparse
 import urllib.parse
+import sys
 from typing import Optional, List, Any, Dict, Set
 
 cat_api = "https://cat.eduroam.org/user/API.php"
-cat_download_api = "https://cat.eduroam.org/user/API.php"
 
 
-def get_old_discovery_from_file(filename: str) -> Optional[Dict]:
-    try:
-        with open(filename, "r") as fh:
-            return json.load(fh)
-    except json.decoder.JSONDecodeError as e:
-        print(e)
-        return None
-    except FileNotFoundError as e:
-        print(e)
-        return None
+def getProfilesFromCat() -> Dict:
+	idp_names: Set[str] = set()
+	instances: List[Any] = []
+
+	r_List_everything = requests.get(
+		cat_api + "?action=listIdentityProvidersWithProfiles"
+	)
+	data = r_List_everything.json()
+	assert "status" in data
+	assert "data" in data
+	assert data["status"] == 1
+	return data["data"]
 
 
-def discovery_needs_refresh(old_discovery: Optional[Dict], new_discovery: Dict) -> bool:
-    if old_discovery is None:
-        return True
+def getLanguagesForCountry(country: str) -> List[str]:
+	d: Dict[str, List[str]] = {
+		"AL": ["sq"],
+		"AM": ["hy"],
+		"AR": ["es"],
+		"AT": ["de", "sl"],
+		"AU": ["en"],
+		"BD": ["bn"],
+		"BE": ["nl", "fr"],
+		"BG": ["bg"],
+		"BR": ["pt"],
+		"CA": ["en", "fr"],
+		"CH": ["de", "fr", "it", "rm"],
+		"CL": ["es"],
+		"CO": ["es", "en"],
+		"CR": ["es"],
+		"CZ": ["cs"],
+		"DE": ["de"],
+		"DK": ["dk", "nb", "sv"],
+		"EC": ["es"],
+		"EE": ["et"],
+		"ES": ["es", "ca"],
+		"FI": ["fi", "sv", "dk", "nb"],
+		"FR": ["fr"],
+		"GE": ["ka", "ab"],
+		"GEANT": [],
+		"GR": ["el"],
+		"HR": ["hr"],
+		"HU": ["hu"],
+		"IE": ["en"],
+		"IL": ["he"],
+		"IS": ["is", "dk", "nb", "sv"],
+		"IT": ["it"],
+		"JP": ["jp"],
+		"KE": ["sw", "en"],
+		"KR": ["ko"],
+		"LK": ["si", "ta"],
+		"LT": ["lt"],
+		"LU": ["fr", "de"],
+		"LV": ["lv"],
+		"MA": ["arb", "zgh"],
+		"MD": ["ro"],
+		"ME": ["cnr"],
+		"MK": ["mk", "sq"],
+		"MT": ["mt", "en"],
+		"MW": ["en", "ny"],
+		"MX": ["es"],
+		"MY": ["zsm", "en", "zh"],
+		"NG": ["nb"],
+		"NL": ["nl"],
+		"NO": ["nb", "dk", "sv"],
+		"NZ": ["en", "mi"],
+		"OM": ["arb"],
+		"PE": ["es"],
+		"PH": ["fil", "en"],
+		"PK": ["ur", "en"],
+		"PL": ["pl"],
+		"PT": ["pt"],
+		"RO": ["ro"],
+		"RS": ["rs"],
+		"SE": ["sv", "dk", "nb"],
+		"SG": ["en", "ms", "zh", "ta"],
+		"SI": ["sl"],
+		"SK": ["sk"],
+		"TR": ["tr"],
+		"UA": ["uk"],
+		"UG": ["en", "sw"],
+		"UK": ["en"],
+		"US": ["en", "es"],
+		"UY": ["es"],
+		"ZA": ["en", "af", "zu", "xh", "nso", "tn", "st", "ts", "ss", "ve", "nr"],
+		"ZM": ["en", "ny", "bem"],
 
-    old_instances = old_discovery["instances"] if "instances" in old_discovery else None
-    new_instances = new_discovery["instances"] if "instances" in new_discovery else None
-
-    assert isinstance(new_instances, List)
-    if not isinstance(old_instances, List) or old_instances is None or new_instances is None:
-        return True
-
-    return not old_instances == new_instances
-
-
-def store_file(discovery: List, filename: str):
-    with open(filename, "w") as fh:
-        json.dump(
-            discovery,
-            fh,
-            separators=(",", ":"),
-            allow_nan=False,
-            sort_keys=True,
-            ensure_ascii=True,
-        )
-        fh.write("\r\n")
-
-
-def get_preferred_name(names: List, country: str) -> Optional[str]:
-    if len(names) == 0:
-        return None
-    lang = {}
-    for name in names:
-        lang[name["lang"]] = name["value"]
-    if "C" in lang:
-        return lang["C"]
-    elif country.lower() in lang:
-        return lang[country.lower()]
-    elif "en" in lang:
-        return lang["en"]
-    else:
-        return names[0]["value"]
-
-
-def get_profiles(idp: Dict) -> List:
-    profiles = []
-    if "profiles" in idp:
-        profile_default = True
-        for profile in idp["profiles"]:
-            profile_name = get_preferred_name(profile["names"], idp["country"])
-            if profile_name is None:
-                profile_name = get_preferred_name(idp["names"], idp["country"])
-            if profile["redirect"]:
-                if "#letswifi" in profile["redirect"]:
-                    # todo: visit .well-known/letswifi.json for actual endpoints
-
-                    # Get base URL for OAuth flow
-                    parsed_url = urllib.parse.urlparse(profile["redirect"])._replace(fragment="")
-                    parsed_url = parsed_url._replace(path=parsed_url.path.rstrip("/"))
-                    profiles.append(
-                        {
-                            "id": "letswifi_cat_%s" % (profile["id"]),
-                            "name": profile_name,
-                            "default": profile_default,
-                            "eapconfig_endpoint": parsed_url._replace(path=parsed_url.path + "/api/eap-config/").geturl(),
-                            "token_endpoint": parsed_url._replace(path=parsed_url.path + "/oauth/token/").geturl(),
-                            "authorization_endpoint": parsed_url._replace(path=parsed_url.path + "/oauth/authorize/").geturl(),
-                            "oauth": True,
-                        }
-                    )
-                    profile_default = False
-                else:
-                    profiles.append(
-                        {
-                            "id": "cat_%s" % (profile["id"]),
-                            "redirect": profile["redirect"],
-                            "name": profile_name,
-                        }
-                    )
-            else:
-                profiles.append(
-                    {
-                        "id": "cat_%s" % (profile["id"]),
-                        "name": profile_name,
-                        "eapconfig_endpoint": "%s?action=downloadInstaller&device=eap-generic&profile=%s" % (cat_download_api, profile["id"]),
-                        "oauth": False,
-                    }
-                )
-    return profiles
+		"AE": [],
+		"KS": [],
+	}
+	if not country in d:
+		print("Country %s unknown" % country, file=sys.stderr)
+	return d[country] if country in d else []
 
 
-def generate(old_seq: int = None):
-    candidate_seq = int(datetime.datetime.utcnow().strftime("%Y%m%d00"))
-    if old_seq is None:
-        # Use a high number so we have a better chance to be over
-        # Let's hope this doesn't happen more than once a day ;)
-        seq = candidate_seq + 80
-    elif candidate_seq <= old_seq:
-        seq = old_seq + 1
-    else:
-        seq = candidate_seq
-
-    return {
-        "version": 1,
-        "seq": seq,
-        "instances": instances(),
-    }
+def getFirstCommonMember(list1: List[str], list2: List[str]) -> Optional[str]:
+	for h in list1:
+		if h in list2:
+			return h
+	return None
 
 
-def instances() -> List:
-    idp_names: Set[str] = set()
-    instances: List[Any] = []
-
-    r_List_everything = requests.get(cat_api + "?action=listIdentityProvidersWithProfiles")
-    data = r_List_everything.json()["data"]
-
-    for idp in data:
-        idp_name = get_preferred_name(data[idp]["names"], data[idp]["country"])
-        if idp_name is None:
-            raise ValueError(f"EntityID {data[idp]['entityID']} has no names")
-
-        # Filter out duplicates
-        if idp_name in idp_names:
-            found = False
-            for previous_idp in instances:
-                if previous_idp["name"] == idp_name and previous_idp["country"] != data[idp]["country"]:
-                    previous_idp["name"] = "%s [%s]" % (
-                        idp_name,
-                        previous_idp["country"],
-                    )
-                    found = True
-            if found:
-                idp_name = "%s [%s]" % (idp_name, data[idp]["country"])
-            else:
-                pass  # it's a duplicate within it's own country
-
-        idp_names.add(idp_name)
-
-        geo = []
-        if "geo" in data[idp]:
-            for coords in data[idp]["geo"]:
-                geo.append(
-                    {
-                        "lon": round(float(coords["lon"]), 3),
-                        "lat": round(float(coords["lat"]), 3),
-                    }
-                )
-
-        profiles = get_profiles(data[idp])
-
-        if profiles:
-            instances.append(
-                {
-                    "id": "cat_%s" % data[idp]["entityID"],
-                    "name": idp_name,
-                    "country": data[idp]["country"],
-                    "cat_idp": int(data[idp]["entityID"]),
-                    "geo": geo,
-                    "profiles": profiles,
-                }
-            )
-
-    return sorted(instances, key=lambda idp: idp["name"])
+def getLocalisedName(
+	names: List, preferredLanguage: str, country: str
+) -> Optional[str]:
+	if len(names) == 0:
+		return None
+	lang: Dict[str, str] = {}
+	for name in names:
+		lang[name["lang"]] = name["value"]
+	if preferredLanguage in lang:
+		return lang[preferredLanguage]
+	elif "C" in lang:
+		return lang["C"]
+	elif "any" in lang:
+		return lang["any"]
+	elif (
+		countryLang := getFirstCommonMember(
+			getLanguagesForCountry(country), list(lang.keys())
+		)
+	) in lang:
+		return lang[countryLang]
+	elif "en" in lang:
+		return lang["en"]
+	else:
+		return names[0]["value"]
 
 
-def parse_args() -> Dict[str, str]:
-    parser = argparse.ArgumentParser(description="Generate geteduroam discovery files")
-    parser.add_argument(
-        "--file-path",
-        nargs="?",
-        metavar="FILE",
-        dest="file_path",
-        default="discovery.json",
-        help="path where to write V1 discovery file to fileystem",
-    )
-    parser.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Force writing file even if nothing changed",
-    )
-    return vars(parser.parse_args())
+def checkProfile(profile: Dict):
+	return profile.name is not None and country is not None and profiles
+
+
+def checkInstitution(profile: Dict):
+	return profile.name is not None and country is not None and profiles
+
+
+def generateInstitution(instData: Dict[str, Any], lang: str):
+	return {
+		"name": getLocalisedName(instData["names"], lang, instData["country"]),
+		"country": instData["country"],
+		"geo": list(
+			map(lambda x: geoCompress(x), instData["geo"] if "geo" in instData else [])
+		),
+		"profiles": list(
+			filter(
+				map(
+					lambda x: generateProfile(
+						x,
+						lang,
+						instData["country"],
+					),
+					instData["profiles"],
+				),
+				lambda profile: checkProfile(profile)
+			)
+		),
+	}
+
+
+def generateProfile(catProfile: Dict, lang: str, country: str) -> Dict[str, str]:
+	if catProfile["redirect"]:
+		if "#letswifi" in catProfile["redirect"]:
+			letswifi_url = urllib.parse.urlparse(catProfile["redirect"])._replace(
+				fragment=""
+			)
+			return {
+				"id": "cat_profile_%s" % catProfile["id"],
+				"name": getLocalisedName(catProfile["names"], lang, country),
+				"type": "letswifi",
+				"letswifi_endpoint": letswifi_url.geturl(),
+			}
+		else:
+			return {
+				"id": "cat_profile_%s" % catProfile["id"],
+				"name": getLocalisedName(catProfile["names"], lang, country),
+				"type": "portal",
+				"portal_endpoint": catProfile["redirect"],
+			}
+	else:
+		return {
+			"id": "cat_profile_%s" % catProfile["id"],
+			"name": getLocalisedName(catProfile["names"], lang, country),
+			"type": "eap-config",
+			"eapconfig_endpoint": "%s?action=downloadInstaller&device=eap-generic&profile=%s"
+			% (cat_api, catProfile["id"]),
+			"mobileconfig_endpoint": "%s?action=downloadInstaller&device=apple_global&profile=%s"
+			% (cat_api, catProfile["id"]),
+		}
+
+
+def geoCompress(geo: Dict) -> Dict:
+	return {
+		"lon": round(float(geo["lon"]), 3),
+		"lat": round(float(geo["lat"]), 3),
+	}
+
+
+def generateDiscovery(catData: Dict, lang: str):
+	return list(
+		map(
+			lambda x: generateInstitution(x[1], lang) | {"id": "cat_idp_%s" % x[0]},
+			filter(lambda x: "profiles" in x[1], catData.items()),
+		)
+	)
+
+
+def parseArgs() -> Dict[str, str]:
+	parser = argparse.ArgumentParser(description="Generate geteduroam discovery files")
+	parser.add_argument(
+		"--file-path",
+		nargs="?",
+		metavar="FILE",
+		dest="file_path",
+		default=None,
+		help="path where to write V2 discovery file to fileystem",
+	)
+	parser.add_argument(
+		"-l",
+		"--language",
+		nargs="?",
+		dest="lang",
+		default="any",
+		help="generate file for this language code",
+	)
+	return vars(parser.parse_args())
 
 
 if __name__ == "__main__":
-    args = parse_args()
-
-    old_discovery = get_old_discovery_from_file(args["file_path"])
-    if not old_discovery or not "seq" in old_discovery:
-        old_discovery = None
-
-    discovery = generate(old_seq=old_discovery["seq"] if old_discovery else None)
-    if not old_discovery or args["force"] or discovery_needs_refresh(old_discovery, discovery):
-        print("Storing discovery seq %s" % discovery["seq"])
-        store_file(discovery, args["file_path"])
-
-    else:
-        print("Unchanged %d" % old_discovery["seq"])
+	args = parseArgs()
+	discovery = generateDiscovery(getProfilesFromCat(), args["lang"])
+	file = (
+		"discovery-%s.json" % args["lang"]
+		if args["file_path"] == None
+		else args["file_path"]
+	)
+	with open(file, "w") as fh:
+		json.dump(
+			discovery,
+			fh,
+			separators=(",", ":"),
+			allow_nan=False,
+			sort_keys=True,
+			ensure_ascii=True,
+			check_circular=False,
+		)
+		fh.write("\r\n")
